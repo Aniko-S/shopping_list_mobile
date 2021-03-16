9fa25715014e1a7e86188994538f45b681ab8a31
Aniko <s.aniko9424@gmail.com>
2021-03-16 20:52:18 +0100

    Sorting list: bought items are at the and of the list.

--- ItemList.js
+++ ItemList.js
@@ -5,13 +5,19 @@ import OneItem from "./OneItem";
 function ItemList({ items, handlePress }) {
   return (
     <View>
-      {items.map((item) => (
-        <OneItem
-          key={item.id}
-          item={item}
-          handlePress={() => handlePress(item.id)}
-        />
-      ))}
+      {items
+        .sort((item1, item2) => {
+          if (item1.bought && !item2.bought) return 1;
+          if (item2.bought && !item1.bought) return -1;
+          return 0;
+        })
+        .map((item) => (
+          <OneItem
+            key={item.id}
+            item={item}
+            handlePress={() => handlePress(item.id)}
+          />
+        ))}
     </View>
   );
 }
